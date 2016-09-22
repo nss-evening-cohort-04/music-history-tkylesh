e9fb9de98eb2ca7784c842ff2ec10d0d1eca4b79
Taylor Shuff <tkylesh@gmail.com>
2016-09-22 13:43:12 -0500

    completed Part One of Music history 4 exercise

--- songs.js
+++ songs.js
@@ -12,38 +12,50 @@ Students must use JavaScript to create a list of songs in the index.html file fo
 
 */
 
-var songs = [];
-//adding a song to the beginning
-songs[0] = "Love Song - by The Cure on the album disintegration"
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-//adding a song to the end of the array
-songs[songs.length] = "aliens exist - by Blink182 on the album enema of the state";
+// var songs = [];
+// //adding a song to the beginning
+// songs[0] = "Love Song - by The Cure on the album disintegration"
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+// //adding a song to the end of the array
+// songs[songs.length] = "aliens exist - by Blink182 on the album enema of the state";
+var songs;
 
+function fileLoad(){
+//parses the string received from the xhr request into an actual js object 
+//so it can be worked with
+	songs = JSON.parse(this.responseText);
+	displayMusicList();
+	};
+
+
+
+function displayMusicList(){	
 //string builder for text to display in main content section of index.html
-var stringbuilder = "<h3>Songs</h3><ul>"
+var stringbuilder = "<h3>Songs</h3><ul>";
 //element to add stringbuilder to
 var songElement = document.getElementById("listsongs");
 
-function displayMusicList(){
 //loop over the array and remove any words or characters that dont belong
-for (var i =0; i < songs.length; i++){
+for (var i =0; i < songs.songlist.length; i++){
 	//replacing all unwanted characters in the array items
-			songs[i] = songs[i].replace(">","-");
+			// songs.songlist[i] = songs.songlist[i].replace(">","-");
 		
-			songs[i] = songs[i].replace("*","");
+			// songs.songlist[i] = songs.songlist[i].replace("*","");
 		
-			songs[i] = songs[i].replace("@","");
+			// songs.songlist[i] = songs.songlist[i].replace("@","");
 
-			songs[i] = songs[i].replace("(","");
+			// songs.songlist[i] = songs.songlist[i].replace("(","");
 
-			songs[i] = songs[i].replace("!","");
+			// songs.songlist[i] = songs.songlist[i].replace("!","");
 
 	//adding array items to stringbuilder variable
-	stringbuilder += "<li>"+songs[i]+"</li></br></br>";
+	stringbuilder += "<li id='song-"+i+"'>"+"title: "+songs.songlist[i].title+", artist: "+
+	songs.songlist[i].artist+", album: "+songs.songlist[i].album +" <button type='button' id='button-"+
+	i+"'>Delete</button></li></br></br>";
 
 
 	}//end of for loop
@@ -52,7 +64,8 @@ for (var i =0; i < songs.length; i++){
 	stringbuilder +="</ul>";
 	songElement.innerHTML = stringbuilder;
 };
-displayMusicList();
+
+
 
 ////////////////////////////////
 //////Code for Add Music View///////////////////
@@ -68,7 +81,6 @@ displayMusicList();
 	</div>`;
 
 	document.getElementById('addsongs').style.display="none";
-
 
 
 
@@ -88,12 +100,30 @@ displayMusicList();
 // you should collect all values from the input fields, 
 // add the song to your array of songs, and update the song list in the DOM.
 	document.getElementById('addBtn').addEventListener("click",function(){
-		var song = document.getElementById('songName').value;
+		var title = document.getElementById('songName').value;
 		var artist = document.getElementById('artistName').value;
 		var album = document.getElementById('albumName').value;
-		songs[songs.length] = `${song} by ${artist} off of ${album}`;
+		var newSongObject= {"artist":artist,"title":title,"album":album};
+		songs.songlist.push(newSongObject);
+		alert("song has been temporarily added to the list music view");
+		// console.log(songs.songlist[(songs.songlist.length-1)]);
 		displayMusicList();
-		console.log(songs[(songs.length-1)]);
+		document.getElementById('songName').value="";
+		document.getElementById('artistName').value="";
+		document.getElementById('albumName').value="";
 	});
 
+var songRequest = new XMLHttpRequest();
+songRequest.addEventListener("load",fileLoad);
+songRequest.open("GET","songs.json");
+songRequest.send();
 
+	//function to delete a song from list after it's been added to the dom
+	document.getElementById('listsongs').addEventListener('click',function(e){
+		console.log("target",e.target);
+		console.log("target",e.target.parentNode);
+		var target = e.target;
+		if(document.getElementById(target.id).innerText === "Delete"){
+		target.parentNode.parentNode.removeChild(target.parentNode);
+		}
+	});
