73d42189d6f4bcf807af9a89c2471f9b9cc1e695
Taylor Shuff <tkylesh@gmail.com>
2016-09-22 15:06:10 -0500

    completed music-history-tkylesh version-4 exercise : added delete buttons and more

--- songs.js
+++ songs.js
@@ -1,73 +1,63 @@
-/*
-Requirements
-
-Use JavaScript arrays, loops, and innerHTML to show the music you love.
-
-Students must use JavaScript to create a list of songs in the index.html file for their Music History project. Have them download the songs.js file, which contains an array of strings with song information.
-
--Each student must add one song to the beginning and the end of the array.
--Loop over the array and remove any words or characters that obviously don't belong.
--Students must find and replace the > character in each item with a - character.
--Must add each string to the DOM in index.html in the main content area.
-
-*/
-
-// var songs = [];
-// //adding a song to the beginning
-// songs[0] = "Love Song - by The Cure on the album disintegration"
-// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-// //adding a song to the end of the array
-// songs[songs.length] = "aliens exist - by Blink182 on the album enema of the state";
+//////////////******************////////////////////////////////
+// These Variables will contain the parsed objects returned/////
+// from their respective XHR calls//////////////////////////////
 var songs;
+var moresongs;
+///////////////*****************************************////////
+////////////////////////////////////////////////////////////////
+
 
 function fileLoad(){
 //parses the string received from the xhr request into an actual js object 
 //so it can be worked with
 	songs = JSON.parse(this.responseText);
 	displayMusicList();
-	};
+};
 
 
-
-function displayMusicList(){	
 //string builder for text to display in main content section of index.html
-var stringbuilder = "<h3>Songs</h3><ul>";
+var stringbuilder;
 //element to add stringbuilder to
 var songElement = document.getElementById("listsongs");
 
+function displayMusicList(){
+stringbuilder = "<h3>Songs</h3><ul>";	
 //loop over the array and remove any words or characters that dont belong
 for (var i =0; i < songs.songlist.length; i++){
 	//replacing all unwanted characters in the array items
-			// songs.songlist[i] = songs.songlist[i].replace(">","-");
-		
+			// songs.songlist[i] = songs.songlist[i].replace(">","-");	
 			// songs.songlist[i] = songs.songlist[i].replace("*","");
-		
 			// songs.songlist[i] = songs.songlist[i].replace("@","");
-
 			// songs.songlist[i] = songs.songlist[i].replace("(","");
-
 			// songs.songlist[i] = songs.songlist[i].replace("!","");
-
 	//adding array items to stringbuilder variable
 	stringbuilder += "<li id='song-"+i+"'>"+"title: "+songs.songlist[i].title+", artist: "+
 	songs.songlist[i].artist+", album: "+songs.songlist[i].album +" <button type='button' id='button-"+
 	i+"'>Delete</button></li></br></br>";
-
-
 	}//end of for loop
-
 	//ending unorder list in stringbuilder
-	stringbuilder +="</ul>";
+	stringbuilder +="</ul><br/><br/>";
+	stringbuilder +="<button type='button' id='moreSongsBtn'>More ></button><br/><br/>";
 	songElement.innerHTML = stringbuilder;
 };
 
 
+//function that will execute when more button is clicked
+function showMore(){
+	moresongs =  JSON.parse(this.responseText);
+	console.log(moresongs);
+	document.getElementById('moreSongsBtn').addEventListener("click",function(){
+		songElement.innerHTML ="";
+		stringbuilder = "";
+		for (var i =0; i < moresongs.songlist.length; i++){
+		songs.songlist.push(moresongs.songlist[i]);
+		};
+		displayMusicList();
+	})
+};
 
-////////////////////////////////
+
+/////////////////////////////////////////////////
 //////Code for Add Music View///////////////////
 	var addView = document.getElementById('addsongs');
 	addView.innerHTML =`<div id="addMusicForm">
@@ -79,10 +69,7 @@ for (var i =0; i < songs.songlist.length; i++){
 			<button type="button" id="addBtn">Add</button><br/>
 		</div>
 	</div>`;
-
 	document.getElementById('addsongs').style.display="none";
-
-
 
 	//function to show add music view
 	function addMusic(){
@@ -90,12 +77,17 @@ for (var i =0; i < songs.songlist.length; i++){
 		document.getElementById('addsongs').style.display="inline-block";
 		document.getElementById('sidebar').style.display="none";
 	}
+
 	//function to show list music view
 	function listMusic(){
 		document.getElementById('listsongs').style.display="inline-block";
 		document.getElementById('sidebar').style.display="inline-block";
 		document.getElementById('addsongs').style.display="none";
 	}
+///////////////*****************************************//////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+
 // Once the user fills out the song form and clicks the add button, 
 // you should collect all values from the input fields, 
 // add the song to your array of songs, and update the song list in the DOM.
@@ -113,15 +105,22 @@ for (var i =0; i < songs.songlist.length; i++){
 		document.getElementById('albumName').value="";
 	});
 
+
 var songRequest = new XMLHttpRequest();
 songRequest.addEventListener("load",fileLoad);
 songRequest.open("GET","songs.json");
 songRequest.send();
 
+var moreSongsRequest = new XMLHttpRequest();
+moreSongsRequest.addEventListener('load',showMore);
+moreSongsRequest.open("GET","moresongs.json");
+moreSongsRequest.send();
+
+
 	//function to delete a song from list after it's been added to the dom
 	document.getElementById('listsongs').addEventListener('click',function(e){
-		console.log("target",e.target);
-		console.log("target",e.target.parentNode);
+		// console.log("target",e.target);
+		// console.log("target",e.target.parentNode);
 		var target = e.target;
 		if(document.getElementById(target.id).innerText === "Delete"){
 		target.parentNode.parentNode.removeChild(target.parentNode);
