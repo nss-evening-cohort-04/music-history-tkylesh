3c17dd4c01cbcba00fb0d9feb8afd71e1d972474
Taylor Shuff <tkylesh@gmail.com>
2016-10-26 08:35:12 -0500

    started converted songs.js to json.  only thing left is the ajax requests.

--- songs.js
+++ songs.js
@@ -1,8 +1,12 @@
+"use strict";
+
+
+$(document).ready(()=> {
 //////////////******************////////////////////////////////
 // These Variables will contain the parsed objects returned/////
 // from their respective XHR calls//////////////////////////////
-var songs;
-var moresongs;
+let songs;
+let moresongs;
 ///////////////*****************************************////////
 ////////////////////////////////////////////////////////////////
 
@@ -18,7 +22,7 @@ function fileLoad(){
 //string builder for text to display in main content section of index.html
 var stringbuilder;
 //element to add stringbuilder to
-var songElement = document.getElementById("listsongs");
+var songElement = $("#listsongs");
 
 function displayMusicList(){
 stringbuilder = "<h3>Songs</h3><ul>";	
@@ -38,7 +42,7 @@ for (var i =0; i < songs.songlist.length; i++){
 	//ending unorder list in stringbuilder
 	stringbuilder +="</ul><br/><br/>";
 	stringbuilder +="<button type='button' id='moreSongsBtn'>More ></button><br/><br/>";
-	songElement.innerHTML = stringbuilder;
+	songElement.html(stringbuilder);
 };
 
 
@@ -46,8 +50,8 @@ for (var i =0; i < songs.songlist.length; i++){
 function showMore(){
 	moresongs =  JSON.parse(this.responseText);
 	console.log(moresongs);
-	document.getElementById('moreSongsBtn').addEventListener("click",function(){
-		songElement.innerHTML ="";
+	$('#moreSongsBtn').on("click",function(){
+		songElement.html("");
 		stringbuilder = "";
 		for (var i =0; i < moresongs.songlist.length; i++){
 		songs.songlist.push(moresongs.songlist[i]);
@@ -59,8 +63,8 @@ function showMore(){
 
 /////////////////////////////////////////////////
 //////Code for Add Music View///////////////////
-	var addView = document.getElementById('addsongs');
-	addView.innerHTML =`<div id="addMusicForm">
+	var addView = $('#addsongs');
+	addView.html(`<div id="addMusicForm">
 	<h3>Add Song</h3><br/>
 		<div>
 			<label class="lbl">title:<input type="text" id="songName"/></label><br/>
@@ -68,21 +72,21 @@ function showMore(){
 			<label class="lbl">album:<input type="text" id="albumName"/></label><br/><br/>
 			<button type="button" id="addBtn">Add</button><br/>
 		</div>
-	</div>`;
-	document.getElementById('addsongs').style.display="none";
+	</div>`);
+	$('#addsongs').css("display","none");
 
 	//function to show add music view
 	function addMusic(){
-		document.getElementById('listsongs').style.display="none";
-		document.getElementById('addsongs').style.display="inline-block";
-		document.getElementById('sidebar').style.display="none";
+		$('#listsongs').css("display","none");
+		$('#addsongs').css("display","inline-block");
+		$('#sidebar').css("display","none");
 	}
 
 	//function to show list music view
 	function listMusic(){
-		document.getElementById('listsongs').style.display="inline-block";
-		document.getElementById('sidebar').style.display="inline-block";
-		document.getElementById('addsongs').style.display="none";
+		$('#listsongs').css("display","inline-block");
+		$('#sidebar').css("display","inline-block");
+		$('#addsongs').css("display","none");
 	}
 ///////////////*****************************************//////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
@@ -91,21 +95,21 @@ function showMore(){
 // Once the user fills out the song form and clicks the add button, 
 // you should collect all values from the input fields, 
 // add the song to your array of songs, and update the song list in the DOM.
-	document.getElementById('addBtn').addEventListener("click",function(){
-		var title = document.getElementById('songName').value;
-		var artist = document.getElementById('artistName').value;
-		var album = document.getElementById('albumName').value;
+	$('#addBtn').on("click",function(){
+		var title = $('#songName').val();
+		var artist = $('#artistName').val();
+		var album = $('#albumName').val();
 		var newSongObject= {"artist":artist,"title":title,"album":album};
 		songs.songlist.push(newSongObject);
 		alert("song has been temporarily added to the list music view");
 		// console.log(songs.songlist[(songs.songlist.length-1)]);
 		displayMusicList();
-		document.getElementById('songName').value="";
-		document.getElementById('artistName').value="";
-		document.getElementById('albumName').value="";
+		$('#songName').val("");
+		$('#artistName').val("");
+		$('#albumName').val("");
 	});
-
-
+//TO DO:
+//convert ajax requests to jquery
 var songRequest = new XMLHttpRequest();
 songRequest.addEventListener("load",fileLoad);
 songRequest.open("GET","songs.json");
@@ -118,7 +122,7 @@ moreSongsRequest.send();
 
 
 	//function to delete a song from list after it's been added to the dom
-	document.getElementById('listsongs').addEventListener('click',function(e){
+	$('#listsongs').on('click',function(e){
 		// console.log("target",e.target);
 		// console.log("target",e.target.parentNode);
 		var target = e.target;
@@ -126,3 +130,4 @@ moreSongsRequest.send();
 		target.parentNode.parentNode.removeChild(target.parentNode);
 		}
 	});
+});
